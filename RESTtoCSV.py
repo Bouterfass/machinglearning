bcaa5860c334beb8e742628c6aebede545d9758a
bouterfass <cefyou28@gmail.com>
2019-02-24 12:11:18 +0100

    changement Data.py /fromCSVtoDATA.py/testData.py

--- RESTtoCSV.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-import csv
-
-
-## wsh la street ca va ou kwa
--- testData copie.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-from Data import Data
-print('Bonjour')
-data1 = Data("philippe","etiquette","millions",205)
-print(data1.type)
